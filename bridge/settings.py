2191a4fb79d2c127e751a0c21002e7b211884765
Chung Kao <chungckao@gmail.com>
2022-08-23 16:18:03 +0800

    Prepare for deployment on heroku

--- bridge/settings.py
+++ bridge/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 from decouple import config
 
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
+
+# Heroku settings
+django_heroku.settings(locals())
