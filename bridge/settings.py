c5a3ff71f279233fcb0e0cb10620d75e093f85cd
Chung Kao <chungckao@gmail.com>
2022-11-11 17:06:33 +0800

    Revise to hard-code ALLOWED_HOST

--- bridge/settings.py
+++ bridge/settings.py
@@ -28,13 +28,14 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="my_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
+ALLOWED_HOST = ['https://jtc-bridgeapp.onrender.com']
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
