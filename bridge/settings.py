7d5ae507c7a452648182d93fc36b0bece9697b4d
Chung Kao <chungckao@gmail.com>
2022-11-22 16:23:43 +0800

    Try DEBUG = True on deploy for trouble shooting

--- bridge/settings.py
+++ bridge/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="my_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'RENDER' in os.environ
 
 ALLOWED_HOSTS = []
 
