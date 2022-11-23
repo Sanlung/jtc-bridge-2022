7d7cc81442a52c4a52be353a20fc289df6e4c582
Chung Kao <chungckao@gmail.com>
2022-11-23 16:44:52 +0800

    Change DEBUG to True so the app deploys pending further investigation of deployment issue

--- bridge/settings.py
+++ bridge/settings.py
@@ -28,13 +28,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="my_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'jtc-bridgeapp.onrender.com']
+ALLOWED_HOSTS = []
 
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
