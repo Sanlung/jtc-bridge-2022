c1ef28418a2ddada06a68e6535d79a56ad0f45d2
Chung Kao <chungckao@gmail.com>
2022-11-25 14:48:50 +0800

    Correct misplaced '/' in static file links in templates to trouble shoot deployment issue

--- bridge/settings.py
+++ bridge/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="my_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
