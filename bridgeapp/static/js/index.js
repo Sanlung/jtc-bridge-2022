f8f850eb2ebdd3d6da1423255ba9b1c2f22ab51c
Chung Kao <chungckao@gmail.com>
2023-02-03 22:33:35 +0800

    Add data-migration file 0005 to create superuser (for deployment database)

--- bridgeapp/static/js/index.js
+++ bridgeapp/static/js/index.js
@@ -7,13 +7,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
         function checkScroll() {
             scrollpos = window.scrollY;
-            if (scrollpos > 450) {
+            if (scrollpos > 100) {
                 navbar.className =
                     "navbar navbar-expand-md navbar-dark bg-dark fixed-top shadow-lg";
-
-            } else if (scrollpos > 100) {
-                navbar.className =
-                    "navbar navbar-expand-md navbar-light bg-light fixed-top";
             } else {
                 navbar.className =
                     "navbar navbar-expand-md navbar-dark fixed-top";
